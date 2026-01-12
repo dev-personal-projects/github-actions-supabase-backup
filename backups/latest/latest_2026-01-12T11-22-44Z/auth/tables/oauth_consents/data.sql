644c3d2b096d4fae3e0f8944c65900881936df82
cm-collins <cm-collins@users.noreply.github.com>
2026-01-12 11:25:12 +0000

    Backup: Supabase database backup - $(date -u +'%Y-%m-%d %H:%M:%S UTC')

---
+++ backups/latest/latest_2026-01-12T11-22-44Z/auth/tables/oauth_consents/data.sql
@@ -0,0 +1,35 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict FCyXGX4Mrj33Va05a10TevwaLCCsKBuYJzbhULle8Xyn4iIU7drf13AgTgQmQl0
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.7 (Ubuntu 17.7-3.pgdg24.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: oauth_consents; Type: TABLE DATA; Schema: auth; Owner: -
+--
+
+COPY auth.oauth_consents (id, user_id, client_id, scopes, granted_at, revoked_at) FROM stdin;
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict FCyXGX4Mrj33Va05a10TevwaLCCsKBuYJzbhULle8Xyn4iIU7drf13AgTgQmQl0
+
