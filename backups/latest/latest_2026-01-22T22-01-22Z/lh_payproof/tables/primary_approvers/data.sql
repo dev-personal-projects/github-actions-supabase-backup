7192975227c82b22432e6c762c38b6c9e83b3e87
cm-collins <cm-collins@users.noreply.github.com>
2026-01-22 22:04:09 +0000

    Backup: Supabase database backup - $(date -u +'%Y-%m-%d %H:%M:%S UTC')

---
+++ backups/latest/latest_2026-01-22T22-01-22Z/lh_payproof/tables/primary_approvers/data.sql
@@ -0,0 +1,46 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict LMMsVE3aG1GwXhN3gTeSX6v3qlljY0uPGjUP6aOuRE8qyk81raLgNtZkSN30jz1
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.7 (Ubuntu 17.7-3.pgdg24.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: primary_approvers; Type: TABLE DATA; Schema: lh_payproof; Owner: -
+--
+
+COPY lh_payproof.primary_approvers (primary_approver_id, primary_email, first_name, middle_name, last_name, contact_description, mobile_number, postal_address, street_address_line1, street_address_line2, city, state, postal_code, country, notes, created_at, updated_at) FROM stdin;
+1	collins.munene@shipht.it	Collins	\N	Munene	Finance Department Distribution List	\N	\N	\N	\N	\N	\N	\N	Kenya	\N	2026-01-22 11:05:37.00851+00	2026-01-22 11:05:37.00851+00
+2	dev-collins@outlook.com	Collins	\N	Munene	Program Manager	\N	\N	\N	\N	\N	\N	\N	Kenya	\N	2026-01-22 11:05:37.00851+00	2026-01-22 11:05:37.00851+00
+3	duane@shipht.it	Duane	\N	Kingoriah	Program Manager	\N	\N	\N	\N	\N	\N	\N	Kenya	\N	2026-01-22 11:05:37.00851+00	2026-01-22 11:05:37.00851+00
+4	finance@shipht.it	\N	\N	\N	Finance Department Distribution List	\N	\N	\N	\N	\N	\N	\N	Kenya	\N	2026-01-22 11:05:37.00851+00	2026-01-22 11:05:37.00851+00
+\.
+
+
+--
+-- Name: primary_approvers_primary_approver_id_seq; Type: SEQUENCE SET; Schema: lh_payproof; Owner: -
+--
+
+SELECT pg_catalog.setval('lh_payproof.primary_approvers_primary_approver_id_seq', 4, true);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict LMMsVE3aG1GwXhN3gTeSX6v3qlljY0uPGjUP6aOuRE8qyk81raLgNtZkSN30jz1
+
---
+++ backups/latest/latest_2026-01-22T22-01-22Z/storage/tables/s3_multipart_uploads_parts/data.sql
@@ -0,0 +1,35 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict xhD3iwqq5smxWa1gX8npnnen8wqY2xOoiQnHfvPMUoZeZu7M9k2jas7oFBrekvv
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.7 (Ubuntu 17.7-3.pgdg24.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: s3_multipart_uploads_parts; Type: TABLE DATA; Schema: storage; Owner: -
+--
+
+COPY storage.s3_multipart_uploads_parts (id, upload_id, size, part_number, bucket_id, key, etag, owner_id, version, created_at) FROM stdin;
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict xhD3iwqq5smxWa1gX8npnnen8wqY2xOoiQnHfvPMUoZeZu7M9k2jas7oFBrekvv
+
