62eade9cc5a104dcc63c7f8f0617f55fcd4f3476
cm-collins <cm-collins@users.noreply.github.com>
2026-01-27 22:03:32 +0000

    Backup: Supabase database backup - $(date -u +'%Y-%m-%d %H:%M:%S UTC')

---
+++ backups/latest/latest_2026-01-27T22-01-42Z/lh_billing_agile/tables/invoice_order_forms/data.sql
@@ -0,0 +1,42 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict nc2VL1bg4gFNEpA06Qm7gFetP3x4ZmdjonaTAEcQmbnGoRdDjN9aJ0tppskeuBe
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.7 (Ubuntu 17.7-3.pgdg24.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: invoice_order_forms; Type: TABLE DATA; Schema: lh_billing_agile; Owner: -
+--
+
+COPY lh_billing_agile.invoice_order_forms (invoice_order_form_id, invoice_id, ofrm_id, source_period_start, source_period_end, created_at, updated_at) FROM stdin;
+\.
+
+
+--
+-- Name: invoice_order_forms_invoice_order_form_id_seq; Type: SEQUENCE SET; Schema: lh_billing_agile; Owner: -
+--
+
+SELECT pg_catalog.setval('lh_billing_agile.invoice_order_forms_invoice_order_form_id_seq', 1, false);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict nc2VL1bg4gFNEpA06Qm7gFetP3x4ZmdjonaTAEcQmbnGoRdDjN9aJ0tppskeuBe
+
---
+++ backups/latest/latest_2026-01-27T22-01-42Z/lh_billing_agile/tables/invoice_sequences/data.sql
@@ -0,0 +1,43 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict w8hfeYQ71djofAIETF0XkbODWZ1LgXQPQthgewvMUmF92CmKrJ7uMaLIoWi7wGW
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.7 (Ubuntu 17.7-3.pgdg24.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: invoice_sequences; Type: TABLE DATA; Schema: lh_billing_agile; Owner: -
+--
+
+COPY lh_billing_agile.invoice_sequences (invoice_sequence_id, context_key, prefix, last_number, format, created_at, updated_at) FROM stdin;
+1	default	INV	9000	{prefix}-{year}-{number:06}	2026-01-27 13:12:18.808563+00	2026-01-27 13:12:18.808563+00
+\.
+
+
+--
+-- Name: invoice_sequences_invoice_sequence_id_seq; Type: SEQUENCE SET; Schema: lh_billing_agile; Owner: -
+--
+
+SELECT pg_catalog.setval('lh_billing_agile.invoice_sequences_invoice_sequence_id_seq', 1, true);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict w8hfeYQ71djofAIETF0XkbODWZ1LgXQPQthgewvMUmF92CmKrJ7uMaLIoWi7wGW
+
