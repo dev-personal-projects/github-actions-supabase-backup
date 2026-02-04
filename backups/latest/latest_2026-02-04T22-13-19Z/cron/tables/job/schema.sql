85e2d82f713b36be9b39ebef4f7de347c97cd362
cm-collins <cm-collins@users.noreply.github.com>
2026-02-04 22:15:24 +0000

    Backup: Supabase database backup - $(date -u +'%Y-%m-%d %H:%M:%S UTC')

---
+++ backups/latest/latest_2026-02-04T22-13-19Z/cron/tables/job/schema.sql
@@ -0,0 +1,27 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict I3eJpk7xfsVvzdiW05CcxAMCJ8gEBFjgWji9ouDsxlT9B3qsUrAoDQgxIc7pJcP
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.7 (Ubuntu 17.7-3.pgdg24.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict I3eJpk7xfsVvzdiW05CcxAMCJ8gEBFjgWji9ouDsxlT9B3qsUrAoDQgxIc7pJcP
+
