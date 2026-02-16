1e0988c48d7cd2f7abd758425bb0493ec7d2aa34
cm-collins <cm-collins@users.noreply.github.com>
2026-02-16 22:18:09 +0000

    Backup: Supabase database backup - $(date -u +'%Y-%m-%d %H:%M:%S UTC')

---
+++ backups/latest/latest_2026-02-16T22-15-04Z/vault/tables/secrets/data.sql
@@ -0,0 +1,35 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict QOb5YkRc6YehLgev9qeKI9Sh5YwmpXp4exeloCTm3WHIF5ECTSdDb86bsIutV1L
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.8 (Ubuntu 17.8-1.pgdg24.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: secrets; Type: TABLE DATA; Schema: vault; Owner: -
+--
+
+COPY vault.secrets (id, name, description, secret, key_id, nonce, created_at, updated_at) FROM stdin;
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict QOb5YkRc6YehLgev9qeKI9Sh5YwmpXp4exeloCTm3WHIF5ECTSdDb86bsIutV1L
+
