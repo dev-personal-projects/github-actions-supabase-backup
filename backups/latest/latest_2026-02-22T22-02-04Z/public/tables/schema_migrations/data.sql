291c9535cc573b93fcee442f5c54cc17261f2f04
cm-collins <cm-collins@users.noreply.github.com>
2026-02-22 22:04:55 +0000

    Backup: Supabase database backup - $(date -u +'%Y-%m-%d %H:%M:%S UTC')

---
+++ backups/latest/latest_2026-02-22T22-02-04Z/public/tables/schema_migrations/data.sql
@@ -0,0 +1,35 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict K4AkLCO8L4B0M4OoMsEIaDFHZaglGqYChkLZPoYKj5TPssb4o2g9GHfZGigp851
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.8 (Ubuntu 17.8-1.pgdg24.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: schema_migrations; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+COPY public.schema_migrations (version, applied_at) FROM stdin;
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict K4AkLCO8L4B0M4OoMsEIaDFHZaglGqYChkLZPoYKj5TPssb4o2g9GHfZGigp851
+
