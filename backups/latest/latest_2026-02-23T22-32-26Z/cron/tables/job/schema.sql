2f79399f213ffdde092ad4dfcefee2c530eb0524
cm-collins <cm-collins@users.noreply.github.com>
2026-02-23 22:35:50 +0000

    Backup: Supabase database backup - $(date -u +'%Y-%m-%d %H:%M:%S UTC')

---
+++ backups/latest/latest_2026-02-23T22-32-26Z/cron/tables/job/schema.sql
@@ -0,0 +1,27 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict NdgRHdsPf3dY6GNsxirrfvg0ldhI5H8oBlPMUjr2rPgpdUUnKc8SEKMaPwstRkV
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.8 (Ubuntu 17.8-1.pgdg24.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict NdgRHdsPf3dY6GNsxirrfvg0ldhI5H8oBlPMUjr2rPgpdUUnKc8SEKMaPwstRkV
+
---
+++ backups/latest/latest_2026-02-23T22-32-26Z/lh_billing_agile/tables/postprocessing_pipeline_steps/data.sql
@@ -0,0 +1,42 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict t4HEPXlxYIgusXz3gXKDKb3zHPSbu9RS3zdUHbaaAD1cQHVfsSAhEDGDAmXyZjG
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.8 (Ubuntu 17.8-1.pgdg24.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: postprocessing_pipeline_steps; Type: TABLE DATA; Schema: lh_billing_agile; Owner: -
+--
+
+COPY lh_billing_agile.postprocessing_pipeline_steps (postprocessing_pipeline_step_id, postprocessing_pipeline_version_id, step_order, step_key, step_config, created_at, updated_at) FROM stdin;
+\.
+
+
+--
+-- Name: postprocessing_pipeline_steps_postprocessing_pipeline_step__seq; Type: SEQUENCE SET; Schema: lh_billing_agile; Owner: -
+--
+
+SELECT pg_catalog.setval('lh_billing_agile.postprocessing_pipeline_steps_postprocessing_pipeline_step__seq', 2, true);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict t4HEPXlxYIgusXz3gXKDKb3zHPSbu9RS3zdUHbaaAD1cQHVfsSAhEDGDAmXyZjG
+
