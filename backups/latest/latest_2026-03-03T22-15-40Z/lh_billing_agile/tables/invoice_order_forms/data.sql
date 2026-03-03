5b0009aa656454aef6adf971167455385cfd44e7
cm-collins <cm-collins@users.noreply.github.com>
2026-03-03 22:18:37 +0000

    Backup: Supabase database backup - $(date -u +'%Y-%m-%d %H:%M:%S UTC')

---
+++ backups/latest/latest_2026-03-03T22-15-40Z/lh_billing_agile/tables/invoice_order_forms/data.sql
@@ -0,0 +1,42 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict 1L27OYnCE6vc9cgwk6ta7d0hm5JDimKWo6aFB9AAeME0Aganv4bWZDwOvkIsDMO
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.9 (Ubuntu 17.9-1.pgdg24.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: invoice_order_forms; Type: TABLE DATA; Schema: lh_billing_agile; Owner: -
+--
+
+COPY lh_billing_agile.invoice_order_forms (invoice_order_form_id, invoice_id, ofrm_id, source_period_start, source_period_end, created_at, updated_at) FROM stdin;
+\.
+
+
+--
+-- Name: invoice_order_forms_invoice_order_form_id_seq; Type: SEQUENCE SET; Schema: lh_billing_agile; Owner: -
+--
+
+SELECT pg_catalog.setval('lh_billing_agile.invoice_order_forms_invoice_order_form_id_seq', 12, true);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict 1L27OYnCE6vc9cgwk6ta7d0hm5JDimKWo6aFB9AAeME0Aganv4bWZDwOvkIsDMO
+
---
+++ backups/latest/latest_2026-03-03T22-15-40Z/realtime/tables/messages/data.sql
@@ -0,0 +1,27 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict uXLqjSa3WMy8CUEztdQGckg7Dz5SM24i7wt1Ac7vYHb9yQecss0k8MW1dQRa5nD
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.9 (Ubuntu 17.9-1.pgdg24.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict uXLqjSa3WMy8CUEztdQGckg7Dz5SM24i7wt1Ac7vYHb9yQecss0k8MW1dQRa5nD
+
