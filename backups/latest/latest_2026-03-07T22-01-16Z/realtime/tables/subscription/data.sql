765d3300bc9346e0e8ab9a5b9ead81684b7be003
cm-collins <cm-collins@users.noreply.github.com>
2026-03-07 22:04:02 +0000

    Backup: Supabase database backup - $(date -u +'%Y-%m-%d %H:%M:%S UTC')

---
+++ backups/latest/latest_2026-03-07T22-01-16Z/realtime/tables/subscription/data.sql
@@ -0,0 +1,42 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict wokCaaN7QPE1Lf0jHtjPqdMCTVyYB207tDOrGRR6KWxtBYAq8hzrqht8ihrI41S
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.9 (Ubuntu 17.9-1.pgdg24.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: subscription; Type: TABLE DATA; Schema: realtime; Owner: -
+--
+
+COPY realtime.subscription (id, subscription_id, entity, filters, claims, created_at, action_filter) FROM stdin;
+\.
+
+
+--
+-- Name: subscription_id_seq; Type: SEQUENCE SET; Schema: realtime; Owner: -
+--
+
+SELECT pg_catalog.setval('realtime.subscription_id_seq', 1, true);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict wokCaaN7QPE1Lf0jHtjPqdMCTVyYB207tDOrGRR6KWxtBYAq8hzrqht8ihrI41S
+
