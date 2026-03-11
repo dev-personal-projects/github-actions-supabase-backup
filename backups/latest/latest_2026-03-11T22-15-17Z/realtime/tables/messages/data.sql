41dc201cc6df4ea72787ad138e22eafc8b1c780a
cm-collins <cm-collins@users.noreply.github.com>
2026-03-11 22:17:58 +0000

    Backup: Supabase database backup - $(date -u +'%Y-%m-%d %H:%M:%S UTC')

---
+++ backups/latest/latest_2026-03-11T22-15-17Z/realtime/tables/messages/data.sql
@@ -0,0 +1,27 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict 2pGau94Kh3hI1Utg0o3upsgsVfVub67kf22jGWzkZWleQY2AZcvbhjCsOv3LXEF
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.9 (Ubuntu 17.9-1.pgdg24.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict 2pGau94Kh3hI1Utg0o3upsgsVfVub67kf22jGWzkZWleQY2AZcvbhjCsOv3LXEF
+
---
+++ backups/latest/latest_2026-03-11T22-15-17Z/vault/tables/secrets/schema.sql
@@ -0,0 +1,27 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict i5yv7d6xzZ4q1GykfxRMVoJC64eKvJfmeINBeB1weYpO46E2GoTC7li3Jm1Ecgh
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.9 (Ubuntu 17.9-1.pgdg24.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict i5yv7d6xzZ4q1GykfxRMVoJC64eKvJfmeINBeB1weYpO46E2GoTC7li3Jm1Ecgh
+
