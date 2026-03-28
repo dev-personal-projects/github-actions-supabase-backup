921ad5dbd9e295947aea6c2ab027005fdf16493d
cm-collins <cm-collins@users.noreply.github.com>
2026-03-28 22:16:55 +0000

    Backup: Supabase database backup - $(date -u +'%Y-%m-%d %H:%M:%S UTC')

---
+++ backups/latest/latest_2026-03-28T22-14-56Z/kristol_fpna/tables/snapshot_agile_ordered_revenue_forecast/data.sql
@@ -0,0 +1,35 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict u78tTWSkb2FioYb7bmYnsYLRAkTvcaP2L64QIqi4n7QNnwGJkrEqGbMux1ctP1M
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.9 (Ubuntu 17.9-1.pgdg24.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: snapshot_agile_ordered_revenue_forecast; Type: TABLE DATA; Schema: kristol_fpna; Owner: -
+--
+
+COPY kristol_fpna.snapshot_agile_ordered_revenue_forecast (sequential_month, year, month_number, month, revenue_projection, snapshot_date) FROM stdin;
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict u78tTWSkb2FioYb7bmYnsYLRAkTvcaP2L64QIqi4n7QNnwGJkrEqGbMux1ctP1M
+
