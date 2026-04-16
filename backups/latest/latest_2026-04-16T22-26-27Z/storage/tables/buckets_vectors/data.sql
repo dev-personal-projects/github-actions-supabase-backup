159dd489e0879ff3db54801beca492748585048c
cm-collins <cm-collins@users.noreply.github.com>
2026-04-16 22:28:29 +0000

    Backup: Supabase database backup - $(date -u +'%Y-%m-%d %H:%M:%S UTC')

---
+++ backups/latest/latest_2026-04-16T22-26-27Z/storage/tables/buckets_vectors/data.sql
@@ -0,0 +1,35 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict PnJAPlHbxC84xtiY3XlOo5tKQMve8y1cqMKK5Oaw00e8B0TOod6P9XQdFWMVHXP
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.9 (Ubuntu 17.9-1.pgdg24.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: buckets_vectors; Type: TABLE DATA; Schema: storage; Owner: -
+--
+
+COPY storage.buckets_vectors (id, type, created_at, updated_at) FROM stdin;
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict PnJAPlHbxC84xtiY3XlOo5tKQMve8y1cqMKK5Oaw00e8B0TOod6P9XQdFWMVHXP
+
