4d87e957aa2025dbc0980c5c97f6eaccf6bee38b
cm-collins <cm-collins@users.noreply.github.com>
2026-05-06 22:38:22 +0000

    Backup: Supabase database backup - $(date -u +'%Y-%m-%d %H:%M:%S UTC')

---
+++ backups/latest/latest_2026-05-06T22-36-26Z/storage/tables/buckets_analytics/data.sql
@@ -0,0 +1,35 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict OR3naEEjaJV1J9z2lKHO7bkZLMsQ48d9mTXXocEyVH7kLmRxrS4RKOhojfQBWXj
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.9 (Ubuntu 17.9-1.pgdg24.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: buckets_analytics; Type: TABLE DATA; Schema: storage; Owner: -
+--
+
+COPY storage.buckets_analytics (name, type, format, created_at, updated_at, id, deleted_at) FROM stdin;
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict OR3naEEjaJV1J9z2lKHO7bkZLMsQ48d9mTXXocEyVH7kLmRxrS4RKOhojfQBWXj
+
