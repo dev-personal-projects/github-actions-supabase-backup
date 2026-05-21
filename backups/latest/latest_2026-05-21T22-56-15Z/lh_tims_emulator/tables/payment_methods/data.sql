90c5257b29df6bb3e81e7ad2e12ab59d14b63cb9
cm-collins <cm-collins@users.noreply.github.com>
2026-05-21 22:58:22 +0000

    Backup: Supabase database backup - $(date -u +'%Y-%m-%d %H:%M:%S UTC')

---
+++ backups/latest/latest_2026-05-21T22-56-15Z/lh_tims_emulator/tables/payment_methods/data.sql
@@ -0,0 +1,48 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict 7MQvO2VQDgqo3kpS7mVR17KXzARVQ17ZQCl2TXBhVVyE2XXbHB3gfTzOqj3GI7W
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.10 (Ubuntu 17.10-1.pgdg24.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: payment_methods; Type: TABLE DATA; Schema: lh_tims_emulator; Owner: -
+--
+
+COPY lh_tims_emulator.payment_methods (id, method_index, pay_name, created_at, updated_at) FROM stdin;
+1	0	CASH	2026-02-03 10:27:33.716817+00	2026-02-03 10:27:33.716817+00
+3	2	CHECK	2026-02-03 10:27:33.716817+00	2026-02-03 10:27:33.716817+00
+4	3	TRANSFER	2026-02-03 10:27:33.716817+00	2026-02-03 10:27:33.716817+00
+5	4	VOUCHER	2026-02-03 10:27:33.716817+00	2026-02-03 10:27:33.716817+00
+6	5	COUPON	2026-02-03 10:27:33.716817+00	2026-02-03 10:27:33.716817+00
+2	1	VISA	2026-02-03 10:27:33.716817+00	2026-02-23 08:37:59.377229+00
+\.
+
+
+--
+-- Name: payment_methods_id_seq; Type: SEQUENCE SET; Schema: lh_tims_emulator; Owner: -
+--
+
+SELECT pg_catalog.setval('lh_tims_emulator.payment_methods_id_seq', 6, true);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict 7MQvO2VQDgqo3kpS7mVR17KXzARVQ17ZQCl2TXBhVVyE2XXbHB3gfTzOqj3GI7W
+
---
+++ backups/latest/latest_2026-05-21T22-56-15Z/realtime/tables/messages/data.sql
@@ -0,0 +1,27 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict zsFTBDAfsH54x5hNVACvNeJRdiEfA2bcR1nwPl7u3w8foba9LVnNCWtcPVzB6UP
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.10 (Ubuntu 17.10-1.pgdg24.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict zsFTBDAfsH54x5hNVACvNeJRdiEfA2bcR1nwPl7u3w8foba9LVnNCWtcPVzB6UP
+
