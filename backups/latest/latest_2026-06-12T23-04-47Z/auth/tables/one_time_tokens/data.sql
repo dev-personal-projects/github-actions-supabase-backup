f1a75d9b22d91daac8e35bffe36047a9960cf0f6
cm-collins <cm-collins@users.noreply.github.com>
2026-06-12 23:06:21 +0000

    Backup: Supabase database backup - $(date -u +'%Y-%m-%d %H:%M:%S UTC')

---
+++ backups/latest/latest_2026-06-12T23-04-47Z/auth/tables/one_time_tokens/data.sql
@@ -0,0 +1,35 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict eB1h7D6ML5LJFsba2fg4bBFSky4jEBbam3FYfIJ0ir9cY8dNRXtsnmgm4G3PZpx
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.10 (Ubuntu 17.10-1.pgdg24.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: one_time_tokens; Type: TABLE DATA; Schema: auth; Owner: -
+--
+
+COPY auth.one_time_tokens (id, user_id, token_type, token_hash, relates_to, created_at, updated_at) FROM stdin;
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict eB1h7D6ML5LJFsba2fg4bBFSky4jEBbam3FYfIJ0ir9cY8dNRXtsnmgm4G3PZpx
+
