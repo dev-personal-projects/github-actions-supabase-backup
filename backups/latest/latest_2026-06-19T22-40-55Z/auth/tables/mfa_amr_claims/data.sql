442c6a33887eb9b0d0a77f5d9d9a0b0756e8f308
cm-collins <cm-collins@users.noreply.github.com>
2026-06-19 22:43:19 +0000

    Backup: Supabase database backup - $(date -u +'%Y-%m-%d %H:%M:%S UTC')

---
+++ backups/latest/latest_2026-06-19T22-40-55Z/auth/tables/mfa_amr_claims/data.sql
@@ -0,0 +1,36 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict Ps6dNtnPx0LUdksd08OLuorsRT9ZOaATJ4pRaFtVvfpcKMx9QqcLZUt3qEaEDlw
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.10 (Ubuntu 17.10-1.pgdg24.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: mfa_amr_claims; Type: TABLE DATA; Schema: auth; Owner: -
+--
+
+COPY auth.mfa_amr_claims (session_id, created_at, updated_at, authentication_method, id) FROM stdin;
+c6234ef1-56c1-4e5a-ac31-28e0840fcdeb	2026-05-21 14:17:20.319001+00	2026-05-21 14:17:20.319001+00	email/signup	2d42fd5d-8345-418a-8f15-c9d94c731ef1
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict Ps6dNtnPx0LUdksd08OLuorsRT9ZOaATJ4pRaFtVvfpcKMx9QqcLZUt3qEaEDlw
+
---
+++ backups/latest/latest_2026-06-19T22-40-55Z/lh_tims_emulator/tables/device_state/data.sql
@@ -0,0 +1,44 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict IwkZzYdFzEEFefZYkQJEEBKSKxTj1lmXXbfaz184Bj9rWJPeUgfdwodAzr1eeDi
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.10 (Ubuntu 17.10-1.pgdg24.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: device_state; Type: TABLE DATA; Schema: lh_tims_emulator; Owner: -
+--
+
+COPY lh_tims_emulator.device_state (id, state_key, state_value, created_at, updated_at) FROM stdin;
+5	auto_print	{"value": true}	2026-02-23 08:37:41.770863+00	2026-02-23 08:37:41.770863+00
+1	pin_required	{"value": false}	2026-02-23 08:37:26.437938+00	2026-02-23 11:50:29.102925+00
+\.
+
+
+--
+-- Name: device_state_id_seq; Type: SEQUENCE SET; Schema: lh_tims_emulator; Owner: -
+--
+
+SELECT pg_catalog.setval('lh_tims_emulator.device_state_id_seq', 10, true);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict IwkZzYdFzEEFefZYkQJEEBKSKxTj1lmXXbfaz184Bj9rWJPeUgfdwodAzr1eeDi
+
