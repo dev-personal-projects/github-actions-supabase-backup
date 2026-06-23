7560c64d40b3dec60fad54624b98edac29b21da9
cm-collins <cm-collins@users.noreply.github.com>
2026-06-23 22:59:07 +0000

    Backup: Supabase database backup - $(date -u +'%Y-%m-%d %H:%M:%S UTC')

---
+++ backups/latest/latest_2026-06-23T22-56-02Z/lh_tims_emulator/tables/cash_operations/data.sql
@@ -0,0 +1,44 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict Sfc5Bee9OSgcsMxnfR4jn65Qmf9qbTGqsnwSrNddRi5He6GRGYFQAksEc7kVwR0
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.10 (Ubuntu 17.10-1.pgdg24.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: cash_operations; Type: TABLE DATA; Schema: lh_tims_emulator; Owner: -
+--
+
+COPY lh_tims_emulator.cash_operations (id, operation_type, amount, performed_at, created_at, updated_at) FROM stdin;
+1	0	2000.0000	2026-02-06 11:08:20.577436+00	2026-02-06 11:08:20.584677+00	2026-02-06 11:08:20.584677+00
+2	0	100.0000	2026-02-23 08:37:32.389064+00	2026-02-23 08:37:32.660693+00	2026-02-23 08:37:32.660693+00
+\.
+
+
+--
+-- Name: cash_operations_id_seq; Type: SEQUENCE SET; Schema: lh_tims_emulator; Owner: -
+--
+
+SELECT pg_catalog.setval('lh_tims_emulator.cash_operations_id_seq', 2, true);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict Sfc5Bee9OSgcsMxnfR4jn65Qmf9qbTGqsnwSrNddRi5He6GRGYFQAksEc7kVwR0
+
