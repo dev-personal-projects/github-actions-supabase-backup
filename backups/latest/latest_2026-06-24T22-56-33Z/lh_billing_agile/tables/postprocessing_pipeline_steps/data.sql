5ae218922ff15f4255f6ed4fa1f014514e3466b2
cm-collins <cm-collins@users.noreply.github.com>
2026-06-24 22:57:56 +0000

    Backup: Supabase database backup - $(date -u +'%Y-%m-%d %H:%M:%S UTC')

---
+++ backups/latest/latest_2026-06-24T22-56-33Z/lh_billing_agile/tables/postprocessing_pipeline_steps/data.sql
@@ -0,0 +1,45 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict 9QwHGitUL6vdVtxSb1oIFmADskqLUs4a9juzYpb3J2lfrhAssrDPFIFxSofX112
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.10 (Ubuntu 17.10-1.pgdg24.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: postprocessing_pipeline_steps; Type: TABLE DATA; Schema: lh_billing_agile; Owner: -
+--
+
+COPY lh_billing_agile.postprocessing_pipeline_steps (postprocessing_pipeline_step_id, postprocessing_pipeline_version_id, step_order, step_key, step_config, created_at, updated_at) FROM stdin;
+1	2	1	tims.register_invoice	\N	2026-03-03 15:54:11.44545+00	2026-03-03 15:54:11.44545+00
+2	2	2	pdf.overlay_qr_stamp	\N	2026-03-03 15:54:11.44545+00	2026-03-03 15:54:11.44545+00
+3	2	3	quickbooks.kick_sync	\N	2026-03-03 15:54:11.44545+00	2026-03-03 15:54:11.44545+00
+\.
+
+
+--
+-- Name: postprocessing_pipeline_steps_postprocessing_pipeline_step__seq; Type: SEQUENCE SET; Schema: lh_billing_agile; Owner: -
+--
+
+SELECT pg_catalog.setval('lh_billing_agile.postprocessing_pipeline_steps_postprocessing_pipeline_step__seq', 3, true);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict 9QwHGitUL6vdVtxSb1oIFmADskqLUs4a9juzYpb3J2lfrhAssrDPFIFxSofX112
+
