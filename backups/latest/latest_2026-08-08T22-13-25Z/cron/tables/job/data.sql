0040b5ba0d83b993d3ba3b08c529e15904ad6ed7
cm-collins <cm-collins@users.noreply.github.com>
2026-08-08 22:15:39 +0000

    Backup: Supabase database backup - $(date -u +'%Y-%m-%d %H:%M:%S UTC')

---
+++ backups/latest/latest_2026-08-08T22-13-25Z/cron/tables/job/data.sql
@@ -0,0 +1,35 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict HZXCb9yJAyrcfijh1frCgAHxrVrufmuJN7oynduLqcHmc7BfHIue7YqPlbqHvE1
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.10 (Ubuntu 17.10-1.pgdg24.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: job; Type: TABLE DATA; Schema: cron; Owner: -
+--
+
+COPY cron.job (jobid, schedule, command, nodename, nodeport, database, username, active, jobname) FROM stdin;
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict HZXCb9yJAyrcfijh1frCgAHxrVrufmuJN7oynduLqcHmc7BfHIue7YqPlbqHvE1
+
