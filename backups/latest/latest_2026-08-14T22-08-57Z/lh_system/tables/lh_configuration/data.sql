e8a6c64da8ec35d2032e922879f6942a44b3de87
cm-collins <cm-collins@users.noreply.github.com>
2026-08-14 22:11:35 +0000

    Backup: Supabase database backup - $(date -u +'%Y-%m-%d %H:%M:%S UTC')

---
+++ backups/latest/latest_2026-08-14T22-08-57Z/lh_system/tables/lh_configuration/data.sql
@@ -0,0 +1,42 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict 8Lhj4icWvUTi13WxFxBO10Ap3flNaFLYSbDGRbNvMzxyvXJCV6e6nJ3PSZ8XNld
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.11 (Ubuntu 17.11-1.pgdg24.04+2)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: lh_configuration; Type: TABLE DATA; Schema: lh_system; Owner: -
+--
+
+COPY lh_system.lh_configuration (cfg_id, cfg_service, cfg_key, cfg_value, cmp_id, created_at, updated_at) FROM stdin;
+\.
+
+
+--
+-- Name: lh_configuration_cfg_id_seq; Type: SEQUENCE SET; Schema: lh_system; Owner: -
+--
+
+SELECT pg_catalog.setval('lh_system.lh_configuration_cfg_id_seq', 1, false);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict 8Lhj4icWvUTi13WxFxBO10Ap3flNaFLYSbDGRbNvMzxyvXJCV6e6nJ3PSZ8XNld
+
