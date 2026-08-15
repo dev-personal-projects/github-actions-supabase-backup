54b4b4aa33d3056d0c4d29e28e98477811351f6b
cm-collins <cm-collins@users.noreply.github.com>
2026-08-15 22:07:42 +0000

    Backup: Supabase database backup - $(date -u +'%Y-%m-%d %H:%M:%S UTC')

---
+++ backups/latest/latest_2026-08-15T22-04-50Z/storage/tables/objects/data.sql
@@ -0,0 +1,35 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict 3chKIpinfV2mCaFELkwo6zPMk1XzG6amlgxq9j9E35GZkB5z5ZnRxpAEzxeSrGz
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.11 (Ubuntu 17.11-1.pgdg24.04+2)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: objects; Type: TABLE DATA; Schema: storage; Owner: -
+--
+
+COPY storage.objects (id, bucket_id, name, owner, created_at, updated_at, last_accessed_at, metadata, version, owner_id, user_metadata) FROM stdin;
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict 3chKIpinfV2mCaFELkwo6zPMk1XzG6amlgxq9j9E35GZkB5z5ZnRxpAEzxeSrGz
+
