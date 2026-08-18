faadd8a87a37dd6fca863d01cf9e5435ab691502
cm-collins <cm-collins@users.noreply.github.com>
2026-08-18 22:10:12 +0000

    Backup: Supabase database backup - $(date -u +'%Y-%m-%d %H:%M:%S UTC')

---
+++ backups/latest/latest_2026-08-18T22-08-08Z/cron/tables/job/schema.sql
@@ -0,0 +1,27 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict AY49F0cHqI2UFgrom4IzlDzrtvM5l3L5UpL1h8X7aYeWhUXRfkqFt0iqjf78Fi7
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.11 (Ubuntu 17.11-1.pgdg24.04+2)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict AY49F0cHqI2UFgrom4IzlDzrtvM5l3L5UpL1h8X7aYeWhUXRfkqFt0iqjf78Fi7
+
---
+++ backups/latest/latest_2026-08-18T22-08-08Z/storage/tables/buckets_vectors/data.sql
@@ -0,0 +1,35 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict HLbhBwheCjPqgR73YP8LC5WqBvvqP0mYhhnaeRTumhxqbOXDOOVGMKo4Di45DKt
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.11 (Ubuntu 17.11-1.pgdg24.04+2)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: buckets_vectors; Type: TABLE DATA; Schema: storage; Owner: -
+--
+
+COPY storage.buckets_vectors (id, type, created_at, updated_at) FROM stdin;
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict HLbhBwheCjPqgR73YP8LC5WqBvvqP0mYhhnaeRTumhxqbOXDOOVGMKo4Di45DKt
+
