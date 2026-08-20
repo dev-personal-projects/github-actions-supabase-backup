1f1e7dcfeeed30508cf17b3617a9f71d4880ab37
cm-collins <cm-collins@users.noreply.github.com>
2026-08-20 22:15:08 +0000

    Backup: Supabase database backup - $(date -u +'%Y-%m-%d %H:%M:%S UTC')

---
+++ backups/latest/latest_2026-08-20T22-12-44Z/auth/tables/sso_providers/data.sql
@@ -0,0 +1,35 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict ALaz9xe50awx5IgPRTxk9q9EMZNT0onry2BhBeVPOiAs7Hu2t1yvWvEmbvE6wfC
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.11 (Ubuntu 17.11-1.pgdg24.04+2)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: sso_providers; Type: TABLE DATA; Schema: auth; Owner: -
+--
+
+COPY auth.sso_providers (id, resource_id, created_at, updated_at, disabled) FROM stdin;
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict ALaz9xe50awx5IgPRTxk9q9EMZNT0onry2BhBeVPOiAs7Hu2t1yvWvEmbvE6wfC
+
