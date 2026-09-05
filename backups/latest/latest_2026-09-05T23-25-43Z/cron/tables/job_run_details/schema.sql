ef51c5a0533256d4128e20824d39b26f5a44cef6
cm-collins <cm-collins@users.noreply.github.com>
2026-09-05 23:27:49 +0000

    Backup: Supabase database backup - $(date -u +'%Y-%m-%d %H:%M:%S UTC')

---
+++ backups/latest/latest_2026-09-05T23-25-43Z/cron/tables/job_run_details/schema.sql
@@ -0,0 +1,27 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict ftIYx7O6ltheWkfrzXMnXx8ZeQI4cUNcnGVchMv9f4ZtXF0KSWJBTjr8Tz03d6D
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.11 (Ubuntu 17.11-1.pgdg24.04+2)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict ftIYx7O6ltheWkfrzXMnXx8ZeQI4cUNcnGVchMv9f4ZtXF0KSWJBTjr8Tz03d6D
+
---
+++ backups/latest/latest_2026-09-05T23-25-43Z/storage/tables/buckets_vectors/schema.sql
@@ -0,0 +1,55 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict 5hoX5Rg6eShg680YfFl7Y9eDVEWFs9E6HnExjf8pmXHTjpmixAW60GclI48OwKZ
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.11 (Ubuntu 17.11-1.pgdg24.04+2)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_table_access_method = heap;
+
+--
+-- Name: buckets_vectors; Type: TABLE; Schema: storage; Owner: -
+--
+
+CREATE TABLE storage.buckets_vectors (
+    id text NOT NULL,
+    type storage.buckettype DEFAULT 'VECTOR'::storage.buckettype NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL
+);
+
+
+--
+-- Name: buckets_vectors buckets_vectors_pkey; Type: CONSTRAINT; Schema: storage; Owner: -
+--
+
+ALTER TABLE ONLY storage.buckets_vectors
+    ADD CONSTRAINT buckets_vectors_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: buckets_vectors; Type: ROW SECURITY; Schema: storage; Owner: -
+--
+
+ALTER TABLE storage.buckets_vectors ENABLE ROW LEVEL SECURITY;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict 5hoX5Rg6eShg680YfFl7Y9eDVEWFs9E6HnExjf8pmXHTjpmixAW60GclI48OwKZ
+
